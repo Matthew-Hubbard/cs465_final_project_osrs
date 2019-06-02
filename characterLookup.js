101f377b7b26906ece33fd4f2a9de861fb468c4a
Matthew Hubbard <mrh8@pdx.edu>
2019-06-02 14:47:34 -0700

    Used a proxy to get around CORS. It's a little slow but it works. The results are now displayed on the web page. Tested with a couple of different usernames. Used Object.keys(obj) to loop through each property name when displaying levels.

--- characterLookup.js
+++ characterLookup.js
@@ -10,23 +10,36 @@ function loadCharacter(){
     else {
         console.log("Looking up...");
         //characterName = characterName.charAt(0).toUpperCase() + characterName.slice(1); //make first char uppercase
-        let idURL = "https://oldschool.tools/ajax/hiscore-stats/" + characterName;
-        //let idURL = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + characterName;
+        //had to use proxy to get around CORS
+        let idURL = "https://cors-anywhere.herokuapp.com/" + "https://oldschool.tools/ajax/hiscore-stats/" + characterName;
+        //let idURL = "https://cors-anywhere.herokuapp.com/" + "http://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + characterName;
+        //let idURL = "" + characterName;
         $.ajax({
+            method: "GET",
             url: idURL,
-            type: "GET",
+            dataType: "json",
+            headers: {  'Access-Control-Allow-Origin': '*', "x-requested-with": "x-requested-by" },
+            crossDomain: true,
             success: function(result){
+                console.log(("Success"));
                 console.log(result);
                 // Need to output stats of character...
-                // if(result["status"] === "success") {
-                //     $('#characterResults').html(htmlStr);
-                // }
-                // else {
-                //     $('#characterResults').html("Item not found.");
-                // }
+                if(result["status"] === "success") {
+                    let skillNames = Object.keys(result['stats']);
+                    let i = 0;
+                    for(let key in result['stats']){
+                       htmlStr += "<p>" + skillNames[i] + " : " + result['stats'][key]['level'] + "</p>";
+                       ++i;
+                    }
+                    $('#characterResults').html(htmlStr);
+                }
+                else {
+                    $('#characterResults').html("User not found.");
+                }
             },
             error: function (error) {
-                console.log('Error ${error}');
+                console.log('Error: ');
+                console.log(error);
             }
         });
     }

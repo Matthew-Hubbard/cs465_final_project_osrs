2acfebc50279a07c14baff301303b5867bb7cc2a
wil26 <wil26@pdx.edu>
2019-06-03 16:43:14 -0700

    added comments to the Welcome function

--- welcome.js
+++ welcome.js
@@ -2,6 +2,8 @@ function Welcome(){
     var welcomestr = "Welcome World of Runescape";
     var b = 0;
     var length = welcomestr.length;
+    //create a new span for each letter
+    //this allows for each letter to have its own animation
     for(var i = 0; i<length; ++i){
         var str = $('<span>');
         if(welcomestr[i]===" "){
@@ -10,12 +12,17 @@ function Welcome(){
         else{
         str.append(welcomestr[i]+ "</span>");
         }
-
+        //append the letter to the welcome element.
         $('#welcome').append(str);
         b= b+0.1;
     }
+    //reloop now through the length and add a animation
+    //delay to each letter
     for (var i =0; i<length; ++i){
         var seconds = -i*50+'s';
+        //all elements start the animation at the same time,
+        //but start their bouncing based on their position
+        //in the text 
         $("span").eq(i).css({'-webkit-animation-delay':seconds})
     }
 }
